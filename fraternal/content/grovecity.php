b3e4fe0ffbc31466537ed9e1f4f823eeea3f6179
KenCohen <ken.cohen@emerson.com>
2015-08-10 16:10:37 -0400

    New Petition and New pictures of Grove City

--- fraternal/content/grovecity.php
+++ fraternal/content/grovecity.php
@@ -1,13 +1,28 @@
 	<script type="text/javascript">
 //	  alert("running script");
 	  var galleryarray = new Array();
-	  galleryarray[0] = "image0.jpg";
-	  galleryarray[1] = "image1.jpg";
-	  galleryarray[2] = "image2.jpg";
-	  galleryarray[3] = "image3.jpg";
-	  galleryarray[4] = "image4.jpg";
-	  galleryarray[5] = "image5.jpg";
-	  galleryarray[6] = "image6.jpg";
+	  galleryarray[0] = "image00.jpg";
+	  galleryarray[1] = "image01.jpg";
+	  galleryarray[2] = "image02.jpg";
+	  galleryarray[3] = "image03.jpg";
+	  galleryarray[4] = "image04.jpg";
+	  galleryarray[5] = "image05.jpg";
+	  galleryarray[6] = "image06.jpg";
+	  galleryarray[7] = "image07.jpg";
+	  galleryarray[8] = "image08.jpg";
+	  galleryarray[9] = "image09.jpg";
+	  galleryarray[10] = "image10.jpg";
+	  galleryarray[11] = "image11.jpg";
+	  galleryarray[12] = "image12.jpg";
+	  galleryarray[13] = "image13.jpg";
+	  galleryarray[14] = "image14.jpg";
+	  galleryarray[15] = "image15.jpg";
+	  galleryarray[16] = "image16.jpg";
+	  galleryarray[17] = "image17.jpg";
+	  galleryarray[18] = "image18.jpg";
+	  galleryarray[19] = "image19.jpg";
+	  galleryarray[20] = "image20.jpg";
+	  galleryarray[21] = "image21.jpg";
 	  var curimg=0;
 	  function rotateImages(){
 	     document.getElementById("slideshow").setAttribute("src", "images/ActiveSlideShow/grovecity/"+galleryarray[curimg]);
@@ -15,20 +30,49 @@
 	  }
 	  window.onload=
 	    function(){
-		  setInterval("rotateImages()", 2500);
+		  setInterval("rotateImages()", 3500);
 		}
 	</script>
 <table width="100%">
   <tr>
     <td align="center" colspan="2">
 	  <h1>Our Grove City Location (under construction)</h1>
-	  <img id="slideshow" src="images/ActiveSlideShow/grovecity/image0.jpg" width="600">
+	  <img id="slideshow" src="images/ActiveSlideShow/grovecity/image00.jpg" width="600">
 	</td>
   </tr>
   <tr>
     <td></td>
 	<td><img src="images/PhotoClub.jpg" alt="Photo Club" width="200"><br>
 	    Images courtesy of the Aladdin Photo Club
+	</td>
+  </tr>
+  <tr>
+    <td>
+      <h4>These images are in the slideshow above</h4>
+	  <h2>August 3, 2015</h2>
+	  <a href="images/grovecity/2015-08-03/East end building.jpg">East end of Building</a><br>
+	  <a href="images/grovecity/2015-08-03/East end view.jpg">East end view</a><br>
+	  <a href="images/grovecity/2015-08-03/East lot.jpg">East lot</a><br>
+	  <a href="images/grovecity/2015-08-03/Front of building 8-3-2015.jpg">Front of building</a><br>
+	  <a href="images/grovecity/2015-08-03/Storage Shed 8-3-2015.jpg">Storage Shed</a><br>
+	  <a href="images/grovecity/2015-08-03/West end 2.jpg">West end 2</a><br>
+	  <a href="images/grovecity/2015-08-03/West end of buildingjpg">West end of building</a><br>
+	  <h2>August 9, 2015</h2>
+	  <a href="images/grovecity/2015-08-09/image01.jpg">image01</a><br>
+	  <a href="images/grovecity/2015-08-09/image02.jpg">image02</a><br>
+	  <a href="images/grovecity/2015-08-09/image03.jpg">image03</a><br>
+	  <a href="images/grovecity/2015-08-09/image04.jpg">image04</a><br>
+	  <a href="images/grovecity/2015-08-09/image05.jpg">image05</a><br>
+	  <a href="images/grovecity/2015-08-09/image06.jpg">image06</a><br>
+	  <a href="images/grovecity/2015-08-09/image07.jpg">image07</a><br>
+	  <a href="images/grovecity/2015-08-09/image08.jpg">image08</a><br>
+	  <a href="images/grovecity/2015-08-09/image09.jpg">image09</a><br>
+	  <a href="images/grovecity/2015-08-09/image10.jpg">image10</a><br>
+	  <a href="images/grovecity/2015-08-09/image11.jpg">image11</a><br>
+	  <a href="images/grovecity/2015-08-09/image12.jpg">image12</a><br>
+	  <a href="images/grovecity/2015-08-09/image13.jpg">image13</a><br>
+	  <a href="images/grovecity/2015-08-09/image14.jpg">image14</a><br>
+	  <a href="images/grovecity/2015-08-09/image15.jpg">image15</a><br>
 	</td>
   </tr>
   
